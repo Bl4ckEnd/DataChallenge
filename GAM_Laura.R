447b857716fef7fe98f91be847a62cd9fc02df94
laufuentes <la.fuentesvi@gmail.com>
2023-02-06 10:39:16 +0100

    GAM_Laura modif and Submission_qgamL6 best submission so far

--- GAM_Laura.R
+++ GAM_Laura.R
@@ -25,9 +25,9 @@ mod0 <- qgam(Load ~ WeekDays*Load.1 + BH + Christmas_break
              + Summer_break + DLS
              + s(Load.7) + s(Time) + s(Temp) + s(toy, k =5, bs = "cc", by=as.factor(WeekDays))
              + s(Temp_s99_min, Temp_s99_max)+s(Temp, Time), 
-             data=Data0, qu=0.5)
+             data=Data0[sel_a,], qu=0.5)
 
-qgam.pred <- predict(mod0, Data1)
+qgam.pred <- predict(mod0, Data0[sel_b,])
 rmse(Data0[sel_b,]$Load, qgam.pred)
 summary(mod.qgam)
 
@@ -78,5 +78,50 @@ submit <- read_delim( file="Data/sample_submission.csv", delim=",")
 submit$Load <- qgam.pred
 write.table(submit, file="Data/submission_qgamL3.csv", quote=F, sep=",", dec='.',row.names = F)
 ###WORST SCORE EVER
+
+
+### try the same with qgam - gam with quantile regression (je crée des classes de Restrinctions covid (GRI20) et je fais des fonctions Load.7 by =GRI20)
+GRI20= cut(Data0$GovernmentResponseIndex,breaks=c(-1, 15, 100), labels=c("low", "considerable"))
+Data00 = cbind(Data0,GRI20)
+
+GRI20= cut(Data1$GovernmentResponseIndex,breaks=c(-1, 15, 100), labels=c("low", "considerable"))
+Data11 = cbind(Data1, GRI20)
+
+mod0 <- qgam(Load ~ WeekDays*Load.1 + BH + Christmas_break
+             + Summer_break + DLS 
+             + s(Load.7, k=5, bs = "cr", by = as.factor(GRI20)) + s(Time) + s(Temp) + s(toy, k =5, bs = "cc", by=as.factor(WeekDays))
+             + s(Temp_s99_min, Temp_s99_max)+s(Temp, Time), 
+             data=Data00, qu=0.4)
+
+qgam.pred <- predict(mod0, Data11)
+rmse(Data00$Load[sel_b], qgam.pred)
+
+plot(qgam.pred, type='l')
+lines(Data0$Load[sel_b],type='l', col='red')
+
+# create submission: submission_qgamL5.csv (2 types de fct de GRI)
+submit <- read_delim( file="Data/sample_submission.csv", delim=",")
+submit$Load <- qgam.pred
+write.table(submit, file="Data/submission_qgamL5.csv", quote=F, sep=",", dec='.',row.names = F)
+
+
+
+#######BEST SCORE EVER EVER 755 ---> ROAD TO BE EMPLOYED BY EDF
+### try the same with qgam - gam with quantile regression
+mod0 <- qgam(Load ~ WeekDays*Load.1 + BH + Christmas_break
+             + Summer_break + DLS + s(Temp) + s(Temp_s99_max, Temp_s99_min)
+             + s(Load.7) + s(Time, k=7) + s(toy, k =20, bs = "cc", by=as.factor(WeekDays))
+             + s(Temp, Time, k=20), 
+             data=Data0, qu=0.4)
+
+gam.check(mod0)
+qgam.pred <- predict(mod0, Data1)
+summary(mod0)
+
+#Feat engineering with basis dimension
+# create submission: submission_qgamL6.csv
+submit <- read_delim( file="Data/sample_submission.csv", delim=",")
+submit$Load <- qgam.pred
+write.table(submit, file="Data/submission_qgamL6.csv", quote=F, sep=",", dec='.',row.names = F)
 
 
