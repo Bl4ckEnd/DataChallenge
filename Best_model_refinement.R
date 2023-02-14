5af8a9d08c0c1fdc08f8f5b091a990fb3adcf7ee
laufuentes <la.fuentesvi@gmail.com>
2023-02-14 14:41:07 +0100

    just asked by github

--- Best_model_refinement.R
+++ Best_model_refinement.R
@@ -11,7 +11,6 @@ rmse = function(y, ychap, digits=0){
 
 load("Data/Data0.Rda")
 load("Data/Data1.Rda")
-Data = load("Data/Data0.Rda")
 
 Data0$Time <- as.numeric(Data0$Date)
 
@@ -81,4 +80,76 @@ ssm_em <- predict(ssm_em, X, y, type='model', compute_smooth = TRUE)
 ssm_em <-readRDS("Results/ssm_em.RDS")
 
 gam9.kalman.Dyn.em <- ssm_em$pred_mean%>%tail(nrow(Data1))
+plot(ssm_em, pause=F, window_size = 14, date = Data$Date, sel = sel_b)
+rmse(y=Data1$Load, ychap=gam9.kalman.Dyn.em)
+
+plot(Data1$Date, Data1$Load, type='l')
+lines(Data1$Date, gam9.forecast, col='red')
+lines(Data1$Date, gam9.kalman.static, col='blue')
+lines(Data1$Date, gam9.kalman.Dyn, col='green')
+lines(Data1$Date, gam9.kalman.Dyn.em, col='purple')
+
+############ APPLICATION A TOUT LE JEU DE DONNÉES
+load("Data/Data0.Rda")
+load("Data/Data1.Rda")
+
+Data0$Time <- as.numeric(Data0$Date)
+
+WD = Data0$WeekDays
+index = which(Data0$GovernmentResponseIndex>=70)
+WD[index]="Saturday"
+Data0 = cbind(Data0,WD)
+
+WD = Data1$WeekDays
+index = which(Data1$GovernmentResponseIndex>=70)
+WD[index]="Saturday"
+Data1 = cbind(Data1, WD)
+
+
+
+equation <- "Load ~ Load.1:as.factor(WeekDays) + BH + Christmas_break + Summer_break + DLS + s(Temp) + s(Temp_s99_max, Temp_s99_min)+ s(Load.7) + s(Time, k=7) + s(toy, k =30, bs = 'cc', by=as.factor(WD))+ s(Temp, Time, k=20)"
+
+gam.0<-qgam(equation%>%as.formula, data=Data0, qu=0.4)
+gam.forecast <- predict(gam.0, newdata=Data1)
+
+X <- predict(gam.0, newdata=Data, type='terms')
+
+for (j in 1:ncol(X)){
+  X[,j] <- (X[,j]-mean(X[,j])) / sd(X[,j])
+}
+
+X <- cbind(X,1)
+d <- ncol(X)
+
+y <- Data11$Load
+
+ssm <- viking::statespace(X, y)
+
+ssm_dyn <- viking::select_Kalman_variances(ssm, X[sel_a, ], y[sel_a], q_list = 2^(-30:0), p1 = 1, ncores = 6)
+
+ssm_dyn <- readRDS("Results/ssm_dyn2.RDS")
+
+ssm_dyn <- predict(ssm_dyn, X, y, type='model', compute_smooth = TRUE)
+gam9.kalman.Dyn <- ssm_dyn$pred_mean%>%tail(nrow(Data1))
+rmse(y=Data1$Load, ychap=gam9.kalman.Dyn)
+plot(ssm_dyn, pause=F, window_size = 14, date = Data$Date)
+
+plot(ssm_dyn, pause=F, window_size = 14, date = Data$Date, sel = sel_b)
+
+
+# using expectation-maximization
+ssm_em <- viking::select_Kalman_variances(ssm, X[sel_a,], y[sel_a], method = 'em', n_iter = 10^3,
+                                          Q_init = diag(d), verbose = 10, mode_diag = T)
+ssm_em <- predict(ssm_em, X, y, type='model', compute_smooth = TRUE)
+ssm_em <-readRDS("Results/ssm_em.RDS")
+
+gam9.kalman.Dyn.em <- ssm_em$pred_mean%>%tail(nrow(Data1))
+plot(ssm_em, pause=F, window_size = 14, date = Data$Date, sel = sel_b)
+rmse(y=Data1$Load, ychap=gam9.kalman.Dyn.em)
+
+plot(Data1$Date, Data1$Load, type='l')
+lines(Data1$Date, gam9.forecast, col='red')
+lines(Data1$Date, gam9.kalman.static, col='blue')
+lines(Data1$Date, gam9.kalman.Dyn, col='green')
+lines(Data1$Date, gam9.kalman.Dyn.em, col='purple')
 
