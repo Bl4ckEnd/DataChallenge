151b1949db6130051a21c1b456bdedc3e48c994b
Valentin Gölz <vale.goelz@gmail.com>
2023-02-28 22:06:42 +0100

    new data structure

--- rapport/GovernmentResponseAnalysis.R
+++ rapport/GovernmentResponseAnalysis.R
@@ -20,16 +20,9 @@ p_load <- Data1 %>%
   ggplot() +
   geom_line(aes(Date, Load.1))
 
-p_response + p_load
+p_response / p_load
 
 hist(Data1$GovernmentResponseIndex)
 
-# <50, 50 - 62, >62
-new_Data1 <- Data1 %>% 
-  mutate(GRI_factor=cut(GovernmentResponseIndex, breaks = c(-Inf, 50, 62, Inf), labels = c("none", "medium", "high")))
 
-new_Data0 <- Data0 %>% 
-  mutate(GRI_factor=cut(GovernmentResponseIndex, breaks = c(-Inf, 50, 62, Inf), labels = c("none", "medium", "high")))
 
-save(new_Data0, file = "Data/Data0.Rda")
-save(new_Data1, file = "Data/Data1.Rda")
