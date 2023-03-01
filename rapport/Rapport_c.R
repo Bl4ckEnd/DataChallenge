5610f76cefa02eee4888c08e6b2c9df85003cf7d
laufuentes <la.fuentesvi@gmail.com>
2023-03-01 16:57:32 +0100

    images

--- rapport/Rapport_c.R
+++ rapport/Rapport_c.R
@@ -66,19 +66,17 @@ summary(anova.res)
 ######## lm
 formula <- "Load ~Load.1 + WeekDays + Load.7 + Temp + Temp_s99_max + Temp_s99_min + WeekDays + WD + BH+ toy + Summer_break + DLS + Christmas_break + HI "
 slm <- lm(formula%>%as.formula, data=Data_train)
-pred = predict(slm, newdata=Data_test)
+pred1 = predict(slm, newdata=Data_test)
 
-plot(Data_test$Date, Data_test$Load, type='l', main='lm')
-lines(Data_test$Date,pred, type='l', col='red')
-rmse(Data_test$Load, pred)
+rmse(Data_test$Load, pred1)
 
 #######Polynomial lm
 formula <- "Load ~Load.1 + WeekDays + Load.7 + I(Load.7^2) + Temp + I(Temp^2) + Temp_s99_max + I(Temp_s99_max^2) + Temp_s99_min + WeekDays + WD + BH+ toy + Summer_break + DLS + Christmas_break + HI "
 slm <- lm(formula%>%as.formula, data=Data_train)
 pred = predict(slm, newdata=Data_test) #2153
 
-plot(Data_test$Date,Data_test$Load, type='l', main='polynomial lm')
-lines(Data_test$Date,pred, type='l', col='red')
+plot(Data_test$Date,pred1, type='l', col='red', main='Lm') #lm 
+lines(Data_test$Date, pred, type='l', col='blue') #lm polynomial version
 rmse(Data_test$Load, pred) #2000
 
 
@@ -88,8 +86,7 @@ formule <- "Load ~ Month + Temp_s95_min + Temp_s95_max + HI + TauxPopMovement +T
 rf<- ranger::ranger(formule, data = Data_train, importance =  'permutation')
 rf.forecast <- predict(rf, data = Data_test)$predictions
 
-plot(Data_test$Date,Data_test$Load, type='l', main='rf')
-lines(Data_test$Date,rf.forecast, type='l', col='red')
+plot(Data_test$Date,rf.forecast, type='l', col='red', main='rf')
 rmse(Data_test$Load, rf.forecast)
 
 ####gam
@@ -99,16 +96,14 @@ gam<-gam(equation%>%as.formula, data=Data_train)
 gam.forecast <- predict(gam, newdata=Data_test)
 gam.check(gam)
 
-plot(Data_test$Date,Data_test$Load, type='l', main='gam')
-lines(Data_test$Date,gam.forecast, type='l', col='red')
+plot(Data_test$Date,gam.forecast, type='l', col='red', main='gam')
 rmse(Data_test$Load, gam.forecast)
 
 ####qgam 
 gam9<-qgam(equation%>%as.formula, data=Data_train, qu=0.4)
 gam9.forecast <- predict(gam9, newdata=Data_test)
 
-plot(Data_test$Date,Data_test$Load, type='l', main='qgam')
-lines(Data_test$Date,gam9.forecast, type='l', col='red')
+plot(Data_test$Date,gam9.forecast, type='l', col='red', main='qgam')
 rmse(Data_test$Load, gam9.forecast)
 
 
@@ -116,8 +111,7 @@ rmse(Data_test$Load, gam9.forecast)
 arima.fit <- forecast::Arima(gam9.forecast, order = c(1,1,2), seasonal = c(0,0,2))
 arima.predict <- fitted(arima.fit)
 
-plot(Data_test$Date, Data_test$Load, type='l', main='arima')
-lines(Data_test$Date, arima.predict, col="red")
+plot(Data_test$Date, arima.predict, col="red", main='arima')
 rmse(arima.predict, Data_test$Load)
 
 
@@ -145,8 +139,8 @@ ssm_dyn <- viking::select_Kalman_variances(ssm, X[1:3051,], y[1:3051], q_list = 
 
 ssm_dyn <- predict(ssm_dyn, X, y, type='model', compute_smooth = TRUE)
 gam9.kalman.Dyn <- ssm_dyn$pred_mean%>%tail(nrow(Data_test))
-plot(Data_test$Date, Data_test$Load, type='l')
-lines(Data_test$Date, gam9.kalman.Dyn, type='l', col='red')
+
+plot(Data_test$Date, gam9.kalman.Dyn, type='l', col='red', main='kalman filter')
 rmse(Data_test$Load, gam9.kalman.Dyn) #1670
 
 ####### Pipeline
@@ -213,6 +207,7 @@ refit <- Arima(ts_res_forecast, model=fit.arima.res)
 prevARIMA.res <- tail(refit$fitted, nrow(Data_test))
 gam9.arima.forecast <- gam9.forecast + prevARIMA.res
 
+plot(as.numeric(gam9.arima.forecast), type='l', col='red', main='Pipeline')
 
 #Mini aggregation d'experts 
 experts <- cbind(gam9.forecast, pred,rf.forecast, gam.forecast, arima.predict, gam9.kalman.Dyn, as.numeric(gam9.arima.forecast))%>%as.matrix
@@ -247,20 +242,18 @@ plot(agg)
 
 or <- oracle(Y=Data_test$Load, experts);
 par(mfrow=c(1,1))
-plot(Data_test$Date, Data_test$Load, type='l')
-lines(Data_test$Date, or$prediction, type='l', col='red')
+plot(Data_test$Date, or$prediction, type='l', col='red', main='Expert aggregation')
 lines(Data_test$Date, agg$prediction, type='l', col='orange')
-rmse(agg$prediction[1:274], y=Data_test$Load[1:274]) #949
+rmse(agg$prediction[1:274], y=Data_test$Load[1:274]) #813
 
 ##### SUBMISSION qgamL19 : agg: even though is 720 --> overall score is much better 
-models = c(Data1$Load, pred, rf.forecast, gam.forecast, gam9.forecast, gam9.kalman.Dyn, gam9.arima.forecast, agg$prediction)
+models = c(pred, rf.forecast, gam.forecast, gam9.forecast, gam9.kalman.Dyn, gam9.arima.forecast, agg$prediction)
 K <-ncol(models)
 col <- rev(RColorBrewer::brewer.pal(n = max(min(K,6),4),name = "Spectral"))[1:min(K,6)]
-nom_mod <- c("Real_load", "polynomial_lm", "Random_forest", "gam", "qgam", "kalman","pipeline", "agg_exp")
+nom_mod <- c("polynomial_lm", "Random_forest", "gam", "qgam", "kalman","pipeline", "agg_exp")
 
-plot(Data_test$Date,Data_test$Load, type='l', ylab='Real_load', col="darkblue", main="Prédictions", lwd=4)
 #Polynomial lm
-lines(Data_test$Date,pred, type='l', ylab = "polynomial_lm", col='lightgreen')
+plot(Data_test$Date,pred, type='l', ylab = "polynomial_lm", col='lightgreen', main='Model comparison')
 #Random forest
 lines(Data_test$Date,rf.forecast, type='l',  ylab='rf',col= "darkgreen")
 #gam
